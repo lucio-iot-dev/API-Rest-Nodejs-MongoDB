1f2589f0f7f47bca39842167d576f282843dde90
Lúcio do Nascimento <luciomate95@gmail.com>
2022-07-29 23:19:27 -0300

    Api-Rest-Nodejs-MongoDB

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+// Forma de ler JSON / middlewares
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+app.use(express.json())
+
+// rotas da API
+const personRoutes = require('./routes/personRoutes')
+
+app.use('/person', personRoutes)
+
+// rota inicial / endpoint
+app.get('/', (req, res) => {
+  res.json({ message: 'Oi Express!' })
+})
+
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.yphyqtm.mongodb.net/?retryWrites=true&w=majority`,
+  )
+  .then(() => {
+    console.log('Conectamos ao MongoDB!')
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err))
+
+ //  mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.yphyqtm.mongodb.net/?retryWrites=true&w=majority
